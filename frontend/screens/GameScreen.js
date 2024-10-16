dd3cdbf6ef3012c830052dbe0d1f83b81bcf72bd
madamski9 <maciek.adamski9@gmail.com>
2024-10-16 16:22:24 +0200

    dodano jenge

--- frontend/screens/GameScreen.js
+++ frontend/screens/GameScreen.js
@@ -1,9 +1,39 @@
 import React from "react";
 import { View, Text, Button, TouchableOpacity } from "react-native";
 
+const JengaBlock = ({ block }) => {
+    const isEven = block % 2 === 0;
+    return (
+        <View style={styles.row}>
+            <View style={isEven ? styles.blockEven : styles.blockOdd}>
+            </View>
+            {isEven && (
+                <>
+                    <View style={styles.blockEven}>
+                    </View>
+                    <View style={styles.blockEven}>
+                    </View>
+                </>
+            )}
+        </View>
+    );
+}
+
 const GameScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
+            <JengaBlock block={1} />
+            <JengaBlock block={2} />
+            <JengaBlock block={3} />
+            <JengaBlock block={4} />
+            <JengaBlock block={5} />
+            <JengaBlock block={6} />
+            <JengaBlock block={7} />
+            <JengaBlock block={8} />
+            <JengaBlock block={9} />
+            <JengaBlock block={10} />
+            <JengaBlock block={11} />
+            <JengaBlock block={12} />
         </View>
     );
 }
@@ -17,13 +47,29 @@ const styles = {
         alignItems: 'center',
         backgroundColor: 'white',
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    instructions: {
-        fontSize: 16,
-        marginBottom: 20,
+    blockOdd: {
+        width: 195, 
+        height: 35,
+        backgroundColor: 'rgb(255,205,129)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+        borderColor: 'black',
+        borderRadius: 4,
+    },
+    blockEven: {
+        width: 65,
+        height: 35,
+        backgroundColor: 'rgb(255,205,129)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+        borderColor: 'black',
+        borderRadius: 4,
     },
 }
